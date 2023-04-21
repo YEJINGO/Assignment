26bf35ea76972959744052ce5a9cd2507fd17363
GOYEJIN <yaejingo@gmail.com>
2023-04-21 23:22:48 +0900

    findByIdAndUserId 추가

--- src/main/java/com/sparta/assignment_lv1/repository/NoteRepository.java
+++ src/main/java/com/sparta/assignment_lv1/repository/NoteRepository.java
@@ -1,11 +1,13 @@
 package com.sparta.assignment_lv1.repository;
 
-import com.sparta.assignment_lv1.dto.NoteResponseDto;
 import com.sparta.assignment_lv1.entity.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
     List<Note> findAllByOrderByModifiedAtDesc();
+
+    Optional<Note> findByIdAndUserId(Long id, Long userId);
 }

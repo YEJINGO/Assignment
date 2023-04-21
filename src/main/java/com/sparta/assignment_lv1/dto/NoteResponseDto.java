3e4ade7763dc92a2cea5e00be6bc8fe2e35f99da
GOYEJIN <yaejingo@gmail.com>
2023-04-21 23:24:13 +0900

    createdAt 추가

--- src/main/java/com/sparta/assignment_lv1/dto/NoteResponseDto.java
+++ src/main/java/com/sparta/assignment_lv1/dto/NoteResponseDto.java
@@ -2,17 +2,23 @@ package com.sparta.assignment_lv1.dto;
 import com.sparta.assignment_lv1.entity.Note;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class NoteResponseDto {
 
     private String title;
-    private String userName;
+    private String username;
     private String contents;
+
+    private LocalDateTime createdAt;
+
 
     public NoteResponseDto(Note note) {
         this.title = note.getTitle();
-        this.userName = note.getUserName();
+        this.username = note.getUsername();
         this.contents = note.getContents();
+        this.createdAt = note.getCreatedAt();
     }
 }
 

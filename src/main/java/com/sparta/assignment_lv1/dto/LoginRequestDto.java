e8f952e3cb88983d32a2ad09207866f26d24e1f4
GOYEJIN <yaejingo@gmail.com>
2023-04-21 23:15:14 +0900

    LoginRequestDto 추가

---
+++ src/main/java/com/sparta/assignment_lv1/dto/LoginRequestDto.java
@@ -0,0 +1,14 @@
+package com.sparta.assignment_lv1.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class LoginRequestDto {
+    private String username;
+    private String password;
+
+}

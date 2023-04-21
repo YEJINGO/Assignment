f6e3237285b1815fedb9e7d4d383f69248d6c4db
GOYEJIN <yaejingo@gmail.com>
2023-04-21 23:23:29 +0900

    userName, password 삭제

--- src/main/java/com/sparta/assignment_lv1/dto/NoteRequestDto.java
+++ src/main/java/com/sparta/assignment_lv1/dto/NoteRequestDto.java
@@ -7,8 +7,6 @@ public class NoteRequestDto {
     // 클라이언트에서 넘어오는 title, userNam, contents 를 객체를 통해서 받는다.
 
     private String title;
-    private String userName;
     private String contents;
-    private String password;
 
 }

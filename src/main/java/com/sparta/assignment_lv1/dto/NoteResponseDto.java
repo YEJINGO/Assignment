cd3a60b142e06db129d1ed4dff9e264294869326
GOYEJIN <yaejingo@gmail.com>
2023-04-26 09:08:11 +0900

    게시글에 Comment 가 함께 보이도록 변경

--- src/main/java/com/sparta/assignment_lv1/dto/NoteResponseDto.java
+++ src/main/java/com/sparta/assignment_lv1/dto/NoteResponseDto.java
@@ -1,8 +1,11 @@
 package com.sparta.assignment_lv1.dto;
+import com.sparta.assignment_lv1.entity.Comment;
 import com.sparta.assignment_lv1.entity.Note;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class NoteResponseDto {
@@ -13,12 +16,15 @@ public class NoteResponseDto {
 
     private LocalDateTime createdAt;
 
-
+    private List<CommentResponseDto> commentList;
     public NoteResponseDto(Note note) {
         this.title = note.getTitle();
         this.username = note.getUsername();
         this.contents = note.getContents();
         this.createdAt = note.getCreatedAt();
+        this.commentList = note.getComments().stream()
+                .map(CommentResponseDto::new)
+                .collect(Collectors.toList());;
     }
 }
 

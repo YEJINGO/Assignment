968e77d370ddf8816a1f0d863486c8ebb74ca1ed
GOYEJIN <yaejingo@gmail.com>
2023-04-26 09:09:51 +0900

    Comment 값 추가에 따른 값 변경

--- src/main/java/com/sparta/assignment_lv1/serivce/NoteService.java
+++ src/main/java/com/sparta/assignment_lv1/serivce/NoteService.java
@@ -1,17 +1,20 @@
 package com.sparta.assignment_lv1.serivce;
 
-import com.sparta.assignment_lv1.dto.MsgAndHttpStatusDto;
 import com.sparta.assignment_lv1.dto.NoteRequestDto;
 import com.sparta.assignment_lv1.dto.NoteResponseDto;
+import com.sparta.assignment_lv1.entity.Comment;
 import com.sparta.assignment_lv1.entity.Note;
 import com.sparta.assignment_lv1.entity.User;
+import com.sparta.assignment_lv1.entity.UserRoleEnum;
+import com.sparta.assignment_lv1.enums.CustomException;
+import com.sparta.assignment_lv1.enums.ErrorCode;
 import com.sparta.assignment_lv1.jwt.JwtUtil;
+import com.sparta.assignment_lv1.repository.CommentRepository;
 import com.sparta.assignment_lv1.repository.NoteRepository;
 import com.sparta.assignment_lv1.repository.UserRepository;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,6 +27,7 @@ public class NoteService {
 
     private final NoteRepository noteRepository;
     private final UserRepository userRepository;
+    private final CommentRepository commentRepository;
     private final JwtUtil jwtUtil;
 
 
@@ -41,9 +45,8 @@ public class NoteService {
             }
             User user = userRepository.findByUsername(claims.getSubject()).orElseThrow(
                     //매개변수가 의도치 않는 상황 유발시
-                    () -> new IllegalArgumentException("사용자가 존재하지 않습니다.")
+                    () -> new CustomException(ErrorCode.NOT_FOUND_USER)
             );
-
             Note note = new Note(requestDto, user);
             noteRepository.save(note);
             return note;
@@ -64,72 +67,83 @@ public class NoteService {
 
     public NoteResponseDto getNote(Long id) { // 선택조회
         Note entity = noteRepository.findById(id).orElseThrow(
-                () -> new IllegalArgumentException("해당 게시물이 없습니다. id=" + id)
+                () -> new CustomException(ErrorCode.NOT_FOUND_NOTE)
         );
+        List<Comment> comments = commentRepository.findByNote_IdOrderByModifiedAtDesc(entity.getId());
+        entity.addComment(comments);
+//        List<CommentResponseDto> comments = commentRepository.findAllByOrderByModifiedAtDesc()
+//                .stream()
+//                .map(CommentResponseDto::new)
+//                .collect(Collectors.toList());
+//        return comments;
         return new NoteResponseDto(entity);
     }
-
 
     @Transactional
     public NoteResponseDto updateNote(Long id, NoteRequestDto requestDto, HttpServletRequest request) {
 
         String token = jwtUtil.resolveToken(request); // 토큰 가져오기
+        Claims claims;
+
+        Note note = noteRepository.findById(id).orElseThrow(
+                () -> new CustomException(ErrorCode.NOT_FOUND_UPDATE_NOTE)
+        );
+
 
         if (token != null) {
-            Note note = validate(id, token);
-            note.update(requestDto);
-            return new NoteResponseDto(note);
-        } else
-            return null;
+
+            if (jwtUtil.validateToken(token)) {
+                claims = jwtUtil.getUserInfoFromToken(token);
+            } else {
+                throw new IllegalArgumentException("Token Error");
+            }
+
+            User user = userRepository.findByUsername(claims.getSubject()).orElseThrow(
+                    //매개변수가 의도치 않는 상황 유발시
+                    () -> new CustomException(ErrorCode.NOT_FOUND_USER)
+            );
+
+            if (note.getUser().getId() == user.getId() || user.getRole().equals(UserRoleEnum.ADMIN)) {
+                note.update(requestDto);
+                noteRepository.save(note);
+                return new NoteResponseDto(note);
+            } else {
+                throw new CustomException(ErrorCode.ONLY_CAN_UPDATE);
+            }
+        }
+        return null;
     }
-
-
-//    @Transactional
-//    public Note updateNote(Long id, NoteResponseDto responseDto) {
-//        Note note = noteRepository.findById(id).orElseThrow(
-//                () -> new IllegalArgumentException("아이디가 존재하지 않습니다."));
-//
-//        if (!note.getPassword().equals(requestDto.getPassword())) {
-//            throw new RuntimeException("비밀번호가 일치하지 않습니다.");
-//        }
-//
-//        note.update(responseDto);
-//        return note;
-//    }
 
 
     @Transactional
-    public MsgAndHttpStatusDto deleteNote(Long id, HttpServletRequest request) {
+    public CustomException deleteNote(Long id, HttpServletRequest request) {
 
         String token = jwtUtil.resolveToken(request); // 토큰 가져오기
-
-        if (token != null) {
-            Note note = validate(id, token);
-            noteRepository.delete(note);
-            return new MsgAndHttpStatusDto("게시글 삭제 성공", HttpStatus.OK.value());
-            //이렇게 new 연산자로 생성해주면, 위에서는 따로 DelResponseDto result = new DelResponseDto(); 이런거 안 만들어줘도 됨
-            //return new BoardResponseDto("게시글 삭제 성공", HttpStatus.OK.value());
-
-        } else {
-            return new MsgAndHttpStatusDto("게시글 작성자만 삭제 가능", HttpStatus.OK.value());
-            //return new BoardResponseDto("게시글 작성자만 삭제 가능", HttpStatus.OK.value());
-        }
-    }
-
-    private Note validate(Long id, String token) {
         Claims claims;
-        if (jwtUtil.validateToken(token)) {
-            claims = jwtUtil.getUserInfoFromToken(token);
-        } else {
-            throw new IllegalArgumentException("Token Error");
-        }
-        User user = userRepository.findByUsername(claims.getSubject()).orElseThrow(
-                //매개변수가 의도치 않는 상황 유발시
-                () -> new IllegalArgumentException("사용자가 존재하지 않습니다.")
+
+        Note note = noteRepository.findById(id).orElseThrow(
+                () -> new CustomException(ErrorCode.NOT_FOUND_NOTE)
         );
 
-        Note note = noteRepository.findByIdAndUserId(id, user.getId()).orElseThrow(
-                () -> new IllegalArgumentException("게시글이 존재하지 않습니다."));
-        return note;
+        if (token != null) {
+            if (jwtUtil.validateToken(token)) {
+                claims = jwtUtil.getUserInfoFromToken(token);
+            } else {
+                throw new IllegalArgumentException("Token Error");
+            }
+
+            User user = userRepository.findByUsername(claims.getSubject()).orElseThrow(
+                    //매개변수가 의도치 않는 상황 유발시
+                    () -> new CustomException(ErrorCode.NOT_FOUND_USER)
+            );
+
+            if (note.getUser().getId() == user.getId() || user.getRole().equals(UserRoleEnum.ADMIN)) {
+                noteRepository.delete(note);
+                return new CustomException(ErrorCode.SUCCESS_COMMENT_DELETE);
+            } else
+                return new CustomException(ErrorCode.ONLY_CAN_DELETE);
+        }
+        return null;
     }
 }
+

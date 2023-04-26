d06867745650777607c550ea9bcfd91e0fefd6d7
GOYEJIN <yaejingo@gmail.com>
2023-04-26 09:06:21 +0900

    연관관계 설정

--- src/main/java/com/sparta/assignment_lv1/entity/Note.java
+++ src/main/java/com/sparta/assignment_lv1/entity/Note.java
@@ -2,9 +2,11 @@ package com.sparta.assignment_lv1.entity;
 
 import com.sparta.assignment_lv1.dto.NoteRequestDto;
 import jakarta.persistence.*;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -21,19 +23,32 @@ public class Note extends Timestamped {
     private String username;
     @Column(nullable = false)
     private String contents;
-    @Column
+//    @Column
 //    @JsonIgnore
-    private String password;
+//    private String password;
+//    @Column
+//    private Long userId;
 
-    @Column
-    private Long userId;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "note", cascade = CascadeType.ALL) // mappedBy의 속성값은 외래키의 주인인 상대 Entity 의 필드명을 의미한다. note 는 연관관계 주인이 가지고 있는 필드명이다. -> comment의 user를 의미
+    private List<Comment> comments = new ArrayList<>();
+
+    public void addUser(User user) {
+        this.user = user;
+    }
+
+    public void addComment(List<Comment> comments){
+        this.comments = comments; }
 
     public Note(NoteRequestDto requestDto, User user) {
         this.username = user.getUsername();
         this.title = requestDto.getTitle();
-        this.password = user.getPassword();
+//        this.password = user.getPassword();
         this.contents = requestDto.getContents();
-        this.userId = user.getId();
+        this.user = user;
     }
 
     public void update(NoteRequestDto requestDto) {

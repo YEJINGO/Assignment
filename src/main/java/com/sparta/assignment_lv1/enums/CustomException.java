82ba3380263006249284ec30a196570e5c594021
GOYEJIN <yaejingo@gmail.com>
2023-04-26 14:12:59 +0900

    IllegalArgumentException -> RuntimeException으로 변경

--- src/main/java/com/sparta/assignment_lv1/enums/CustomException.java
+++ src/main/java/com/sparta/assignment_lv1/enums/CustomException.java
@@ -3,7 +3,7 @@ package com.sparta.assignment_lv1.enums;
 import lombok.Getter;
 
 @Getter
-public class CustomException extends IllegalArgumentException{
+public class CustomException extends RuntimeException{
     private ErrorCode errorCode;
 
     public CustomException( ErrorCode errorCode) {

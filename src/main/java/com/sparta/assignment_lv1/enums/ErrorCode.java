cd433ee4f9e9311bbcf0d5bd653694374352e515
GOYEJIN <yaejingo@gmail.com>
2023-04-26 10:15:14 +0900

    SUCCESS_NOTE_DELETE, SUCCESS_COMMENT_DELETE 삭제

--- src/main/java/com/sparta/assignment_lv1/enums/ErrorCode.java
+++ src/main/java/com/sparta/assignment_lv1/enums/ErrorCode.java
@@ -16,19 +16,12 @@ public enum ErrorCode {
     , NOT_FOUND_NOTE(400, "NotFoundNote", "해당 게시물이 없습니다.")
     , NOT_FOUND_UPDATE_NOTE(400, "NotFoundUpdateNote", "수정하고자 하는 게시글이 없습니다.")
     , ONLY_CAN_UPDATE(400, "OnlyCanUpdate", "해당 사용자 혹은 관리자가 아니면 게시글을 수정할 수 없습니다.")
-    , SUCCESS_NOTE_DELETE(200, "SuccessNoteDelete", "게시물이 삭제되었습니다.")
     , ONLY_CAN_DELETE(400, "OnlyCanDelete", "해당 사용자 혹은 관리자가 아니면 게시글을 삭제할 수 없습니다.")
     , INVALID_TOKEN(400, "InvalidToken", "토큰이 유효하지 않습니다.")
     , NOT_FOUND_UPDATE_COMMENT(400, "NotFoundUpdateComment", "수정하고자 하는 댓글이 없습니다.")
     , ONLY_CAN_UPDATE_COMMENT(400, "OnlyCanUpdateComment", "해당 사용자가 작성인 혹은 관리자가 아니라면, 댓글을 수정할 수 없습니다.")
     , NOT_FOUND_Delete_COMMENT(400, "NotFoundDeleteComment", "삭제하고자 하는 댓글이 없습니다.")
     , ONLY_CAN_DELETE_COMMENT(400, "OnlyCanUpdateComment", "해당 사용자가 작성인 혹은 관리자가 아니라면, 댓글을 삭제할 수 없습니다.")
-    , SUCCESS_COMMENT_DELETE(200, "SuccessCommentDelete", "댓굴 삭제 완료")
-
-
-
-
-
     ;
 
     private int statusCode;

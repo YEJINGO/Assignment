e1fb026a7b915d74c0d78d9ef0bd234aa351fcb5
GOYEJIN <yaejingo@gmail.com>
2023-04-26 09:07:31 +0900

    @DeleteMapping 리턴 타입 변경 (MsaAndHttpStatusDto -> CustomException)

--- src/main/java/com/sparta/assignment_lv1/repository/NoteRepository.java
+++ src/main/java/com/sparta/assignment_lv1/repository/NoteRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
     List<Note> findAllByOrderByModifiedAtDesc();
-
     Optional<Note> findByIdAndUserId(Long id, Long userId);
+
+
 }

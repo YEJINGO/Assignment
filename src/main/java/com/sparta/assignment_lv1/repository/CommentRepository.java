36c340167be0a26bbb1a0aa31b880e134406851b
GOYEJIN <yaejingo@gmail.com>
2023-04-26 09:01:43 +0900

    CommentRepository 추가

---
+++ src/main/java/com/sparta/assignment_lv1/repository/CommentRepository.java
@@ -0,0 +1,13 @@
+package com.sparta.assignment_lv1.repository;
+
+import com.sparta.assignment_lv1.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByOrderByModifiedAtDesc();
+
+    List<Comment> findByNote_IdOrderByModifiedAtDesc(Long id);
+
+}

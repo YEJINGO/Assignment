2c14794e8416b3e309a043bd6718eb51a7a3be55
GOYEJIN <yaejingo@gmail.com>
2023-04-29 02:11:04 +0900

    Refactor : 비밀 번호 제약 조건 추가

--- src/main/java/com/sparta/assignment_lv1/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/assignment_lv1/dto/SignupRequestDto.java
@@ -1,5 +1,7 @@
 package com.sparta.assignment_lv1.dto;
 
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,8 +10,11 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class SignupRequestDto {
-
+    @Size(min=4, max=10, message = "아이디는 최소 4자 이상, 10자 이하로 만들어야 합니다.")
+    @Pattern(regexp = "^[a-z0-9]*$") //정규식에서 *$는 문자열의 끝부분에서 0번 이상의 모든 문자를 나타낸다.
     private String username;
+    @Size(min=8, max=15, message = "비밀번호는 최소 8자 이상, 15자 이하로 만들어야 합니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9]*$")
     private String password;
     private boolean admin = false;
     private String adminToken = "";
--- src/main/java/com/sparta/assignment_lv1/entity/User.java
+++ src/main/java/com/sparta/assignment_lv1/entity/User.java
@@ -2,8 +2,6 @@ package com.sparta.assignment_lv1.entity;
 
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,7 +11,7 @@ import lombok.NoArgsConstructor;
  */
 @Getter
 @NoArgsConstructor
-@Entity(name ="users")
+@Entity(name = "users")
 @Embeddable
 public class User {
     @Id
@@ -21,21 +19,23 @@ public class User {
     private Long id;
 
     @Column(nullable = false, unique = true) // null 허용X, 중복 허용 X
-    @Size(min=4, max=10, message = "아이디는 최소 4자 이상, 10자 이하로 만들어야 합니다.")
-    @Pattern(regexp = "^[a-z0-9]*$") //정규식에서 *$는 문자열의 끝부분에서 0번 이상의 모든 문자를 나타낸다.
+//    @Size(min=4, max=10, message = "아이디는 최소 4자 이상, 10자 이하로 만들어야 합니다.")
+//    @Pattern(regexp = "^[a-z0-9]*$") //정규식에서 *$는 문자열의 끝부분에서 0번 이상의 모든 문자를 나타낸다.
     private String username;
     @Column(nullable = false)
 //    @Size(min=8, max=15, message = "비밀번호는 최소 8자 이상, 15자 이하로 만들어야 합니다.")
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,15}$", message="비밀번호를 8~15자로 입력해주세요.(특수문자o)")
+//    @Pattern(regexp = "^[a-zA-Z0-9]*$")
     String password;
 
     @Column(nullable = false)
     @Enumerated(value = EnumType.STRING)
     private UserRoleEnum role;
 
-    public User(String username, String password,UserRoleEnum role) {
+    public User(String username, String password, UserRoleEnum role) {
         this.username = username;
         this.password = password;
         this.role = role;
     }
+
+
 }

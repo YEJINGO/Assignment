cda78c4bd922ceaf77974774c4be58bf7a605f63
GOYEJIN <yaejingo@gmail.com>
2023-04-29 21:42:01 +0900

    Fix : Dto값 추가
    - noteLikeCount 추가

--- src/main/java/com/sparta/assignment_lv1/dto/NoteResponseDto.java
+++ src/main/java/com/sparta/assignment_lv1/dto/NoteResponseDto.java
@@ -1,22 +1,26 @@
 package com.sparta.assignment_lv1.dto;
-import com.sparta.assignment_lv1.entity.Comment;
+
 import com.sparta.assignment_lv1.entity.Note;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Getter
+@RequiredArgsConstructor
 public class NoteResponseDto {
+
 
     private String title;
     private String username;
     private String contents;
 
     private LocalDateTime createdAt;
-
     private List<CommentResponseDto> commentList;
+    private int noteLikeCount;
+
     public NoteResponseDto(Note note) {
         this.title = note.getTitle();
         this.username = note.getUsername();
@@ -24,7 +28,8 @@ public class NoteResponseDto {
         this.createdAt = note.getCreatedAt();
         this.commentList = note.getComments().stream()
                 .map(CommentResponseDto::new)
-                .collect(Collectors.toList());;
+                .collect(Collectors.toList());
+        this.noteLikeCount = note.getLikes().size();
     }
 }
 

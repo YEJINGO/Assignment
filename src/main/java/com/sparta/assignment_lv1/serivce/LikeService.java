187d6253b8867c750b76907312669660ca087bc6
GOYEJIN <yaejingo@gmail.com>
2023-04-29 22:32:32 +0900

    Fix : 반환 타입 변경
    - ResponseEntity -> ResponseEntity<MsgAndHttpStatusDto>

--- src/main/java/com/sparta/assignment_lv1/serivce/LikeService.java
+++ src/main/java/com/sparta/assignment_lv1/serivce/LikeService.java
@@ -1,5 +1,6 @@
 package com.sparta.assignment_lv1.serivce;
 
+import com.sparta.assignment_lv1.dto.MsgAndHttpStatusDto;
 import com.sparta.assignment_lv1.entity.Comment;
 import com.sparta.assignment_lv1.entity.Likes;
 import com.sparta.assignment_lv1.entity.Note;
@@ -10,6 +11,7 @@ import com.sparta.assignment_lv1.repository.LikeRepository;
 import com.sparta.assignment_lv1.repository.NoteRepository;
 import com.sparta.assignment_lv1.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -23,7 +25,7 @@ public class LikeService {
     private final CommentRepository commentRepository;
 
     // 1. 노트 좋아요 개수 추출
-    public ResponseEntity updateNoteLike(Long noteId, UserDetailsImpl userDetails) {
+    public ResponseEntity<MsgAndHttpStatusDto> updateNoteLike(Long noteId, UserDetailsImpl userDetails) {
 
         if (likeRepository.findByNote_IdAndUser_Id(noteId, userDetails.getUser().getId()) == null) {
 
@@ -38,11 +40,12 @@ public class LikeService {
             Likes like = likeRepository.findByNote_IdAndUser_Id(noteId, userDetails.getUser().getId());
             likeRepository.delete(like);
         }
-        return ResponseEntity.ok(likeRepository.findAllByNote_Id(noteId).size());
+        return ResponseEntity.ok(new MsgAndHttpStatusDto("좋아요가 성공적으로 업데이트 되었습니다.", HttpStatus.OK.value()));
+//        return ResponseEntity.ok(likeRepository.findAllByNote_Id(noteId).size());
     }
 
     // 2. 게시물 좋아요 개수 추출
-    public ResponseEntity updateCommentLike(Long commentId, UserDetailsImpl userDetails) {
+    public ResponseEntity<MsgAndHttpStatusDto> updateCommentLike(Long commentId, UserDetailsImpl userDetails) {
 
 
         if (likeRepository.findByComment_IdAndUser_Id(commentId, userDetails.getUser().getId()) == null) {
@@ -57,7 +60,6 @@ public class LikeService {
             Likes like = likeRepository.findByComment_IdAndUser_Id(commentId, userDetails.getUser().getId());
             likeRepository.delete(like);
         }
-        return ResponseEntity.ok(likeRepository.findAllByComment_Id(commentId).size());
+        return ResponseEntity.ok(new MsgAndHttpStatusDto("좋아요가 성공적으로 업데이트 되었습니다.", HttpStatus.OK.value()));
     }
 }
-

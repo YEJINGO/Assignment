5483cf9012c5922e5b1b526cac669cf0f240d527
GOYEJIN <yaejingo@gmail.com>
2023-04-29 21:41:49 +0900

    Fix : Dto값 추가
    - commentLikeCount 추가

--- src/main/java/com/sparta/assignment_lv1/dto/CommentResponseDto.java
+++ src/main/java/com/sparta/assignment_lv1/dto/CommentResponseDto.java
@@ -2,22 +2,25 @@ package com.sparta.assignment_lv1.dto;
 
 import com.sparta.assignment_lv1.entity.Comment;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class CommentResponseDto {
+
 
     private String contents;
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
+    private int commentLikeCount;
 
     public CommentResponseDto(Comment comment) {
         this.contents = comment.getContents();
         this.createdAt = comment.getCreatedAt();
         this.modifiedAt = comment.getModifiedAt();
+        this.commentLikeCount = comment.getLikes().size();
     }
 
 }
